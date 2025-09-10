8742d4b0bda1caccba79b2040a4c4eda6c28cfde
Rebecca Jenkins <rebeccajenkins@Rebeccas-Laptop-3.local>
2025-09-10 00:26:07 -0400

    Added skimr install

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
-
+install.packages("skimr", dependencies = TRUE)
 IRkernel::installspec(user = FALSE)
 
 system("wget https://github.com/quarto-dev/quarto-cli/releases/download/v2.12.0/quarto-2.12.0-linux-amd64.deb")
