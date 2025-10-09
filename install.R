0544619eacecbbf32c1877219fba20f32004dd6a
Rebecca Jenkins <rebeccajenkins@Rebeccas-Laptop-3.local>
2025-10-09 00:25:49 -0400

    Added mclust

--- install.R
+++ install.R
@@ -8,6 +8,7 @@ install.packages("matlab")
 install.packages("GGally")
 install.packages("gridExtra")
 install.packages("ppclust")
+install.packages("mclust")
 IRkernel::installspec(user = FALSE)
 
 system("wget https://github.com/quarto-dev/quarto-cli/releases/download/v2.12.0/quarto-2.12.0-linux-amd64.deb")
